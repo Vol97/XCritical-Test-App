2ed517eb8d9e708f0bd533b23ffe7fa12e4ec023
Serhii Voloshyn <sergey.voloshyn97@gmail.com>
2021-07-21 23:08:18 +0300

    started implementing toolbar and drawer menu

--- app/src/main/java/com/example/xcriticaltestapp/MainTeleprompterActivity.kt
+++ app/src/main/java/com/example/xcriticaltestapp/MainTeleprompterActivity.kt
@@ -1,17 +1,19 @@
 package com.example.xcriticaltestapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.navOptions
 import androidx.navigation.ui.setupWithNavController
 import com.example.xcriticaltestapp.databinding.ActivityMainTeleprompterBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainTeleprompterActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainTeleprompterBinding
+    private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var binding: ActivityMainTeleprompterBinding
     private lateinit var navigationController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,14 +21,62 @@ class MainTeleprompterActivity : AppCompatActivity() {
         binding = ActivityMainTeleprompterBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        initializeListeners()
+
         navigationController = findNavController(R.id.fragmentContainerView)
         binding.bottomNavigationView.setupWithNavController(navigationController)
+
+        toggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            R.string.open_drawer,
+            R.string.close_drawer
+        )
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        initializeListeners()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
     private fun initializeListeners() {
         binding.floatingActionButton.setOnClickListener {
             navigationController.navigate(R.id.createProjectFragment)
+        }
+
+        binding.drawerMenuNavView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.aboutApp -> Toast.makeText(
+                    applicationContext,
+                    "Clicked info", Toast.LENGTH_LONG
+                ).show()
+                R.id.contactSupport -> Toast.makeText(
+                    applicationContext,
+                    "Clicked support", Toast.LENGTH_LONG
+                ).show()
+                R.id.notifications -> Toast.makeText(
+                    applicationContext,
+                    "Clicked notifications", Toast.LENGTH_LONG
+                ).show()
+                R.id.settings -> Toast.makeText(
+                    applicationContext,
+                    "Clicked settings", Toast.LENGTH_LONG
+                ).show()
+                R.id.exit -> Toast.makeText(
+                    applicationContext,
+                    "Clicked exit", Toast.LENGTH_LONG
+                ).show()
+            }
+            true
         }
     }
 }
